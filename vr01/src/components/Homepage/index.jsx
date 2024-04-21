f67ae3b756a2233142d623251e0b4c247d913879
leenock <leenock176@gmail.com>
2024-04-22 00:26:48 +0300

    Hero section update

---
+++ vr01/src/components/Homepage/index.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "../HeroSection/HeroSection";
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <HeroSection />
+    </>
+  )
+}
+
+export default Home
