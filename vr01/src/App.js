bd18da69c5503c60f2ef282856dae1070c0190be
leenock <leenock176@gmail.com>
2024-04-23 00:09:50 +0300

    uniquecards updates

--- vr01/src/App.js
+++ vr01/src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Homepage";
-import About from "./pages/AboutUs/About";
 import Practice from "./pages/practice-area/Practice";
 
 
@@ -15,7 +14,7 @@ function App() {
              <Navbar />
               <Routes>
                 <Route path="/" element={<Home />}></Route>
-                <Route path="/about" element={<About />}></Route>
+                
                 <Route path="/practice-area" element={<Practice />}></Route>
              </Routes>
           </div>
