d3a1fdbbe5e9a20b85f9248b2aedd639361b1382
leenock <leenock176@gmail.com>
2024-04-23 23:40:36 +0300

    vr03 update

---
+++ vr03/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import AboutUs from './components/AboutUs/AboutUs';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <AboutUs />
+    </div>
+  );
+}
+export default App;
